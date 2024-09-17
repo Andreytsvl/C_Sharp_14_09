e4ed057de5155d584df7e9378ee7fd13160e5c7e
unknown <andreytsvl15@gmail.com>
2024-09-17 12:21:52 +0300

    sem_8_DZ_2

--- Program.cs
+++ Program.cs
@@ -526,7 +526,7 @@ using static System.Console;
 //             for (int k = 0; k < inArray.GetLength(2); k++)
 //             {
 //                 Write($"{inArray[i, j, k]} ({i},{j},{k}) ");
-                
+
 //             }
 //             WriteLine();
 //         }
@@ -536,3 +536,109 @@ using static System.Console;
 
 // task 62 DZ Заполните спирально массив 4 на 4.
 Clear();
+Write("Введите размер матрицы ");
+int s = int.Parse(ReadLine());
+int[,] array = GetArray(s);
+PrintArray(array);
+
+int[,] GetArray(int size)
+{
+    int[,] result = new int[size, size];
+    int i = 0;
+    int j = 0;
+    int rowE = size - 1;
+    int columnE = size - 1;
+    int rowS = 0;
+    int columnS = 0;
+
+    bool left = true;
+    bool top = true;
+    int count = 10;
+
+    while (count < size * size+10)
+    {
+        count++;
+        result[i, j] = count;
+        // идём вправо
+        if (left && top)
+        {
+            if (j == columnE)
+            {
+                rowS++;
+                top = true;
+                left = false;
+                i++;
+                continue;
+            }
+            else
+            {
+                j++;
+                continue;
+            }
+        }
+        // идём вниз
+        if (!left && top)
+        {
+            if (i == rowE)
+            {
+                columnE--;
+                top = false;
+                left = false;
+                j--;
+                continue;
+            }
+            else
+            {
+                i++;
+                continue;
+            }
+        }
+        //идём влево
+        if (!left && !top)
+        {
+            if (j == columnS)
+            {
+                rowE--;
+                top = false;
+                left = true;
+                i--;
+                continue;
+            }
+            else
+            {
+                j--;
+                continue;
+            }
+        }
+        // идём вверх
+        if (left && !top)
+        {
+            if (i == rowS)
+            {
+                columnS++;
+                top = true;
+                left = true;
+                j++;
+                continue;
+            }
+            else
+            {
+                i--;
+                continue;
+            }
+        }
+    }
+    return result;
+}
+
+void PrintArray(int[,] inArray)
+{
+    for (int i = 0; i < inArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < inArray.GetLength(1); j++)
+        {
+            Write($"{inArray[i, j]} ");
+        }
+        WriteLine();
+    }
+}

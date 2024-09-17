2c51f818c4be54a45bde35df7aed35174b0f94ae
unknown <andreytsvl15@gmail.com>
2024-09-17 10:50:19 +0300

    sem_8_DZ

--- Program.cs
+++ Program.cs
@@ -271,5 +271,205 @@ using static System.Console;
 //     return f;
 // }
 
+// task 54 DZ Напишите программу, которая упорядочит по убыванию
+// элементы каждой строки двумерного массива.
+// Clear();
+// Write("Введите количество строк массива ");
+// int m = int.Parse(ReadLine());
+// Write("Введите количество столбцов массива ");
+// int n = int.Parse(ReadLine());
+
+// int[,] array = GetArray(m, n, 0, 9);
+// PrintArray(array);
+// WriteLine();
+// NewArray(array);
+// PrintArray(array);
+
+// int[,] GetArray(int rows, int columns, int min, int max)
+// {
+//     int[,] result = new int[rows, columns];
+//     for (int i = 0; i < result.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < result.GetLength(1); j++)
+//         {
+//             result[i, j] = new Random().Next(min, max + 1);
+//         }
+//     }
+//     return result;
+// }
+
+// void PrintArray(int[,] inArray)
+// {
+//     for (int i = 0; i < inArray.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < inArray.GetLength(1); j++)
+//         {
+//             Write($"{inArray[i, j]} ");
+//         }
+//         WriteLine();
+//     }
+// }
+
+// void NewArray(int[,] inArray)
+// {
+
+//     for (int i = 0; i < inArray.GetLength(0); i++)
+//     {
 
 
+//         for (int j = 0; j < inArray.GetLength(1); j++)
+//         {
+//             for (int k = j + 1; k < inArray.GetLength(1); k++)
+//             {
+//                 if (inArray[i, k] > inArray[i, j])
+//                 {
+//                     int temp = inArray[i, j];
+//                     inArray[i, j] = inArray[i, k];
+//                     inArray[i, k] = temp;
+//                 }
+//             }
+//         }
+
+//     }
+
+// }
+
+// task 56 DZ Напишите программу, которая будет находить строку
+// с наименьшей суммой элементов в двумерном массиве
+// Clear();
+
+// Write("Введите количество строк массива ");
+// int m = int.Parse(ReadLine());
+// Write("Введите количество столбцов массива ");
+// int n = int.Parse(ReadLine());
+
+// int[,] array = GetArray(m, n, 0, 9);
+// PrintArray(array);
+// WriteLine();
+// int num = NumberRows(array);
+// WriteLine($"{num} - строка с наименьшей суммой");
+
+// int[,] GetArray(int rows, int columns, int min, int max)
+// {
+//     int[,] result = new int[rows, columns];
+//     for (int i = 0; i < result.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < result.GetLength(1); j++)
+//         {
+//             result[i, j] = new Random().Next(min, max + 1);
+//         }
+//     }
+//     return result;
+// }
+
+// void PrintArray(int[,] inArray)
+// {
+//     for (int i = 0; i < inArray.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < inArray.GetLength(1); j++)
+//         {
+//             Write($"{inArray[i, j]} ");
+//         }
+//         WriteLine();
+//     }
+// }
+
+// int NumberRows(int[,] inArray)
+// {
+//     int row = 0;
+//     int sum = 0;
+//     for (int k = 0; k < inArray.GetLength(1); k++)
+//         {
+//             sum = sum + inArray[0, k];
+//         }
+//     for (int i = 1; i < inArray.GetLength(0); i++)
+//     {
+//         int minSum = 0;
+//         for (int j = 0; j < inArray.GetLength(1); j++)
+//         {
+//             minSum = minSum + inArray[i, j];
+//         }
+//         if (sum>minSum)
+//         {
+//             sum = minSum;
+//             row = i;
+//         }
+//     }
+//     return row;
+// }
+
+// task 58 DZ Задайте 2 матрицы. Напишите программу,
+// которая будет находить их произведение
+// Clear();
+// Write("Введите количество строк массива 1 ");
+// int m1 = int.Parse(ReadLine());
+// Write("Введите количество столбцов массива 1 ");
+// int n1 = int.Parse(ReadLine());
+// Write("Введите количество строк массива 2 ");
+// int m2 = int.Parse(ReadLine());
+// Write("Введите количество столбцов массива 2 ");
+// int n2 = int.Parse(ReadLine());
+
+// int[,] array1 = GetArray(m1, n1, 0, 10);
+// PrintArray(array1);
+// WriteLine();
+// int[,] array2 = GetArray(m2, n2, 0, 10);
+// PrintArray(array2);
+// WriteLine();
+// if (array1.GetLength(1) != array2.GetLength(0))
+// {
+//     WriteLine(" такие матрицы нельзя перемножать");
+//     return;
+// }
+// else
+// {
+// int[,] array3 = GetResultMatrix(array1, array2);
+// PrintArray(array3);
+// }
+
+
+// int[,] GetArray(int rows, int columns, int min, int max)
+// {
+//     int[,] result = new int[rows, columns];
+//     for (int i = 0; i < result.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < result.GetLength(1); j++)
+//         {
+//             result[i, j] = new Random().Next(min, max + 1);
+//         }
+//     }
+//     return result;
+// }
+
+// void PrintArray(int[,] inArray)
+// {
+//     for (int i = 0; i < inArray.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < inArray.GetLength(1); j++)
+//         {
+//             Write($"{inArray[i, j]} ");
+//         }
+//         WriteLine();
+//     }
+// }
+
+// int[,] GetResultMatrix(int[,] matrixA, int[,] matrixB)
+// {
+//     int[,] matrixC = new int[matrixA.GetLength(0), matrixB.GetLength(1)];
+//     for (int i = 0; i < matrixA.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < matrixB.GetLength(1); j++)
+//         {
+//             for (int k = 0; k < matrixA.GetLength(1); k++)
+//             {
+//                 matrixC[i, j] += matrixA[i, k] * matrixB[k, j];
+//             }
+//         }
+
+//     }
+//     return matrixC;
+// }
+
+// task 60 DZ Сформировать трёхмерный массив из неповторяющихся двузначных
+// чисел. Построчно вывести его, добавляя индексы каждого элемента.
+Clear();
